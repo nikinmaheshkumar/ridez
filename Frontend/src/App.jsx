54ff7d7df2fd177a945c331fe8bbbac682f6a0f4
Nikin M <nikinmaheshkumar@gmail.com>
2025-06-18 11:31:45 +0530

    Frontend init

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './styles/index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Ridez</h1>
+        <p className="text-lg text-blue-700">Your ride-sharing app</p>
+      </div>
+    </>
+  )
+}
+
+export default App
