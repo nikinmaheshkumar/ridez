c59e014e427e313b6b074b1ebb581a04a6a7911e
Nikin M <nikinmaheshkumar@gmail.com>
2025-06-18 13:10:12 +0530

    feat login form design

--- Frontend/src/components/LoginForm.jsx
+++ Frontend/src/components/LoginForm.jsx
@@ -1,31 +1,59 @@
 import { useState } from "react";
+import { FaMobileAlt } from "react-icons/fa";
+import { TbLockPassword } from "react-icons/tb";
 
 export default function LoginForm() {
-  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
-    // TODO: Add login API logic
+    console.log("Login attempted with:", {
+      phone,
+      password,
+    });
+    e.target.reset();
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit">Login</button>
+    <form
+      onSubmit={handleLogin}
+      className="flex flex-col gap-4 py-6 px-4 w-full max-w-md mx-auto items-start bg-white "
+      autoComplete="off"
+    >
+      <label className="flex flex-row items-center border rounded-md p-3 w-full">
+        <FaMobileAlt className="h-6 w-6 mr-4 text-[#068fff]" />
+        <input
+          type="tel"
+          className="tabular-nums w-full border-none outline-none text-sm"
+          required
+          placeholder="Phone"
+          pattern="[0-9]*"
+          minLength="10"
+          maxLength="10"
+          title="Must be 10 digits"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </label>
+      <label className="flex flex-row items-center border rounded-md p-3 w-full">
+        <TbLockPassword className="h-6 w-6 mr-4 text-[#068fff]" />
+        <input
+          type="password"
+          required
+          placeholder="Password"
+          minLength="8"
+          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          className="w-full border-none outline-none text-sm"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button
+        type="submit"
+        className="rounded-lg bg-[#068fff] text-white w-full sm:w-32 h-10 self-center hover:bg-[#0577d5] transition"
+      >
+        Login
+      </button>
     </form>
   );
 }
--- Frontend/src/pages/Auth.jsx
+++ Frontend/src/pages/Auth.jsx
@@ -9,11 +9,11 @@ export default function AuthPage() {
   const isRegister = mode === "register";
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#f2f2f2]">
-      <div className=" flex flex-col shadow-lg p-6 rounded-lg bg-white dark:bg-black border border-black dark:border-white mb-4">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#f2f2f2] ">
+      <div className=" flex flex-col shadow-lg p-6 rounded-lg bg-white dark:bg-black border border-black dark:border-white mb-4 max-w-xs sm:min-w-md" >
         <div className="flex flex-row mb-8 gap-2">
-          <button onClick={() => setMode("login")} className={`rounded-lg basis-1/2 text-white h-10 transition ${isLogin ? "bg-black" : "bg-[#0f0f86]" }`}>Login</button>
-          <button onClick={() => setMode("register")} className={`rounded-lg basis-1/2 text-white h-10 transition ${isRegister ? "bg-black" : "bg-[#0f0f86]" }`}>Register</button>
+          <button onClick={() => setMode("login")} className={`rounded-lg basis-1/2 text-white text-lg h-12 transition ${isLogin ? "bg-black" : "bg-[#0f0f86]" }`}>Login</button>
+          <button onClick={() => setMode("register")} className={`rounded-lg basis-1/2 text-white text-lg h-12 transition ${isRegister ? "bg-black" : "bg-[#0f0f86]" }`}>Register</button>
         </div>
         {mode === "login" ? <LoginForm /> : <RegisterForm />}
       </div>
